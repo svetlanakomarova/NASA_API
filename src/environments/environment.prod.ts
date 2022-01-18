13ac7f759c96043d5513c6190e42b450b10c58bd
Svetlana Komarova <97261986+svetlanakomarova@users.noreply.github.com>
2022-01-17 21:20:32 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  NASA_API_KEY: 'YOUR_KEY', //replace YOUR_KEY with actual key
+  NASA_API_KEY: 'YOUR_KEY', //replace YOUR_KEY with an actual key
   NASA_API_URL: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='
 };
